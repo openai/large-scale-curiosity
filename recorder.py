6d91dee289c42130932af5748e2a2964889c19a9
yburda <yburda@openai.com>
2018-08-09 14:27:52 -0700

    Remove unnecessary printing

--- recorder.py
+++ recorder.py
@@ -15,13 +15,8 @@ class Recorder(object):
         self.ext_rews = [[] for _ in range(nenvs)]
         self.ep_infos = [{} for _ in range(nenvs)]
         self.filenames = [self.get_filename(i) for i in range(nenvs)]
-        all_filenames = MPI.COMM_WORLD.allreduce(self.filenames, MPI.SUM)
         if MPI.COMM_WORLD.Get_rank() == 0:
-            logger.info("videos saved to ", self.filenames[0])
-            print(all_filenames)
-            print(len(all_filenames))
-            with open(os.path.join(logger.get_dir(), "all_filenames.pkl"), "wb") as f:
-                pickle.dump(all_filenames, f)
+            logger.info("episode recordings saved to ", self.filenames[0])
 
     def record(self, timestep, lump, acs, infos, int_rew, ext_rew, news):
         for out_index in range(self.nenvs_per_lump):
@@ -41,12 +36,6 @@ class Recorder(object):
                 if news[out_index]:
                     self.ep_infos[in_index]['ret'] = infos[out_index]['episode']['r']
                     self.ep_infos[in_index]['len'] = infos[out_index]['episode']['l']
-                    if 'mz_episode' in infos[out_index]:
-                        self.ep_infos[in_index]['rooms'] = infos[out_index]['mz_episode']['visited_rooms']
-                        self.mz = True
-                    else:
-                        self.mz = False
-                    # assert self.ep_infos[in_index]['ret'] == sum(self.ext_rews[in_index])
                     self.dump_episode(in_index)
 
                 self.acs[in_index].append(acs[out_index])
@@ -65,11 +54,7 @@ class Recorder(object):
         self.ep_infos[i].clear()
 
     def episode_worth_saving(self, i):
-        if self.mz:
-            return (i == 0 and MPI.COMM_WORLD.Get_rank() == 0) or (
-                any(room in self.ep_infos[i]["rooms"] for room in list(range(3, 5)) + list(range(8, 25))))
-        else:
-            return (i == 0 and MPI.COMM_WORLD.Get_rank() == 0)
+        return (i == 0 and MPI.COMM_WORLD.Get_rank() == 0)
 
     def is_first_episode_step(self, i):
         return len(self.int_rews[i]) == 0
